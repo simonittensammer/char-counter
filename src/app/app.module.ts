3986cefb995f92c820981acca46dea195e90557c
siwa_sittensammer <s.ittensammer@siwa.at>
2024-11-13 21:19:50 +0100

    finalize char sort game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {CharSortComponent} from "./char-sort/char-sort.component";
+import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import {CharSortComponent} from "./char-sort/char-sort.component";
     FormsModule,
     CharSortComponent
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/char-sort/char-sort.component.ts
+++ src/app/char-sort/char-sort.component.ts
@@ -1,23 +1,41 @@
 import { Component } from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-char-sort',
   standalone: true,
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    FormsModule
   ],
   templateUrl: './char-sort.component.html',
   styleUrl: './char-sort.component.css'
 })
 export class CharSortComponent {
-  word: string = 'ANGULAR';
+  word: string = '';
   shuffledWord: string[] = [];
   selectedLetters: string[] = [];
+  characterCount: number = 5; // Default character count
+  showAnswer: boolean = false; // Option to show the correct answer
 
-  constructor() {
-    this.shuffleWord();
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchWord();
+  }
+
+  fetchWord() {
+    const apiUrl = `https://random-word-api.herokuapp.com/word?length=${this.characterCount}&lang=de&number=1`;
+    this.http.get<string[]>(apiUrl).subscribe(response => {
+      if (response && response.length > 0) {
+        this.word = response[0].toUpperCase();
+        this.shuffleWord();
+        this.showAnswer = false; // Reset showAnswer when fetching a new word
+      }
+    });
   }
 
   shuffleWord() {
@@ -35,9 +53,23 @@ export class CharSortComponent {
   resetGame() {
     this.selectedLetters = [];
     this.shuffleWord();
+    this.showAnswer = false;
+  }
+
+  getNewWord() {
+    this.selectedLetters = [];
+    this.fetchWord();
   }
 
   isCorrectOrder(): boolean {
     return this.selectedLetters.join('') === this.word;
+  }
+
+  getDisplayWord(): string {
+    return this.word.split('').map((letter, index) => this.selectedLetters[index] ? this.selectedLetters[index] : '_').join(' ');
+  }
+
+  revealAnswer() {
+    this.showAnswer = true;
   }
 }
